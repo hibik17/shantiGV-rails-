5ef1547618e04a125d0a5d143ede6ef2444ac7ba
Hibiki Tatsuno <a8119118@gsuite.si.aoyama.ac.jp>
2021-10-08 17:27:36 +0900

    google 認証完了

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,23 +2,23 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
-         # 以下を追加
-         # google以外の認証をする場合は %i[twitter, facebook]などとなります
-         :omniauthable, omniauth_providers: %i[google_oauth2]
+  :recoverable, :rememberable, :validatable, :omniauthable,
+  omniauth_providers: [:google_oauth2]
 
   protected
-  def self.find_for_google(auth)
-    user = User.find_by(email: auth.info.email)
+  # 以下を追加
+    def self.from_omniauth(access_token)
+      data = access_token.info
+      user = User.where(email: data['email']).first
 
-    unless user
-      user = User.create(name:     auth.info.name,
-                        provider: auth.provider,
-                        uid:      auth.uid,
-                        token:    auth.credentials.token,
-                        password: Devise.friendly_token[0, 20],
-                        meta:     auth.to_yaml)
+      # Uncomment the section below if you want users to be created if they don't exist
+      unless user
+          user = User.create(name: data['name'],
+             email: data['email'],
+             password: Devise.friendly_token[0,20]
+          )
+      end
+      user
     end
-  end
 
 end
