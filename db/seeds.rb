1dc5a7cf33fdbe73d7dbc2f0b69cc88dcbfe1f72
Hibiki Tatsuno <a8119118@gsuite.si.aoyama.ac.jp>
2021-10-15 20:17:47 +0900

    user index

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+100.times do |n|
+  User.create!(
+    name: "test{n}人目",
+    profile: "test！#{n}回目",
+    email: "test#{n}@gmail.com",
+    password: 'pass#{n}'
+    )
+end
