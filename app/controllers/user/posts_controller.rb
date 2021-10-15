c8cfccdebaf2113c0ff7ee599df297d7f8238034
Hibiki Tatsuno <a8119118@gsuite.si.aoyama.ac.jp>
2021-10-15 15:52:39 +0900

    post controller 編集完了

--- app/controllers/user/posts_controller.rb
+++ app/controllers/user/posts_controller.rb
@@ -1,4 +1,8 @@
 class User::PostsController < ApplicationController
+
+  before_action :correct_user, only: [:edit, :update, :delete]
+  before_action :authenticate_user!
+
   def index
     @post = Post.new
   end
@@ -8,6 +12,7 @@ class User::PostsController < ApplicationController
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def create
@@ -22,6 +27,13 @@ class User::PostsController < ApplicationController
   end
 
   def update
+    @post = Post.find(params[:id])
+    if @post.update(post_parameter)
+      flash[:notice] = "投稿の編集が完了しました。"
+      user_post_path(@post)
+    else
+      render :edit
+    end
   end
 
   def delete
@@ -30,5 +42,13 @@ class User::PostsController < ApplicationController
   private
   def post_parameter
     params.require(:post).permit(:title, :content, :genre_id, :country_id, :image)
+  end
+
+  def correct_user
+    @post_user = Post.find(params[:id]).user
+    if @post_user != current_user
+      flash[:notice] = "権限がありません。編集権限を管理者にお問い合わせください。"
+      user_posts_path
+    end
   end
 end
