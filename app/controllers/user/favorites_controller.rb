78119f6c02986faf0f46702b8a0ef15c35603cc4
Hibiki Tatsuno <a8119118@gsuite.si.aoyama.ac.jp>
2021-10-16 11:57:44 +0900

    favorites page実装完了

--- app/controllers/user/favorites_controller.rb
+++ app/controllers/user/favorites_controller.rb
@@ -1,4 +1,13 @@
 class User::FavoritesController < ApplicationController
+  def index
+    @user_fav = Array.new
+    current_user.favorites.each do |fav|
+      @user_fav.push(fav.post)
+    end
+    # kaminariはDBからデータを抽出してかけるため、配列に対してkaminariは使用できない。
+    @paginate_active = false
+  end
+
   def create
     post = Post.find(params[:post_id])
     favorite = current_user.favorites.new(post_id: post.id)
--- db/schema.rb
+++ db/schema.rb
@@ -77,6 +77,8 @@ ActiveRecord::Schema.define(version: 2021_10_15_154031) do
     t.datetime "deleted_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "provider"
+    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
