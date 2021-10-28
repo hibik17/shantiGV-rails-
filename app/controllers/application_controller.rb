5b080972c2b6b014a683a9eb488d9add27d7d7b5
Hibiki Tatsuno <a8119118@gsuite.si.aoyama.ac.jp>
2021-10-28 16:45:04 +0900

    init

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,21 +3,20 @@ class ApplicationController < ActionController::Base
     def after_sign_in_path_for(resouce)
       case resouce
       when Admin
-        admin_users_path
+        admin_top_path
       when User
         user_homes_mypage_path
       end
     end
     def after_sign_out_path_for(resource)
-      binding.pry
       if resource == :admin
-        root_path
+        new_admin_session_path
       else
         root_path
       end
     end
     protected
     def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :line_name, :nick_name, :email])
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :nick_name, :email])
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -84,8 +84,6 @@ ActiveRecord::Schema.define(version: 2021_10_16_154132) do
     t.datetime "deleted_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "provider"
-    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
