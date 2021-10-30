e6b359bdfe9a4c8de12419bf0c8b8b336d4bf798
hibik17 <75942464+hibik17@users.noreply.github.com>
2021-10-30 12:24:21 +0900

    404ページの作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,22 +1,39 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-    def after_sign_in_path_for(resouce)
-      case resouce
-      when Admin
-        admin_top_path
-      when User
-        user_homes_mypage_path
-      end
+  unless Rails.env.production?
+    rescue_from Exception,                      with: :_render_404
+    rescue_from ActiveRecord::RecordNotFound,   with: :_render_404
+    rescue_from ActionController::RoutingError, with: :_render_404
+  end
+  
+  def routing_error
+    raise ActionController::RoutingError, params[:path]
+  end
+  def after_sign_in_path_for(resouce)
+    case resouce
+    when Admin
+      admin_top_path
+    when User
+      user_homes_mypage_path
     end
-    def after_sign_out_path_for(resource)
-      if resource == :admin
-        new_admin_session_path
-      else
-        root_path
-      end
+  end
+
+  def after_sign_out_path_for(resource)
+    if resource == :admin
+      new_admin_session_path
+    else
+      root_path
     end
-    protected
+  end
+
+
+  protected
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :nick_name, :email])
+    end
+
+    # エラーページ作成
+    def _render_404(e = nil)
+      render 'errors/404'
     end
 end
